065e20280f076ca8a6059fb7a1d030da4b2068d1
thanakrit555 <59599161+thanakrit555@users.noreply.github.com>
2021-01-15 15:19:56 +0700

    lab11_4 completed

--- lab11_4.cpp
+++ lab11_4.cpp
@@ -2,6 +2,9 @@
 
 using namespace std;
 
+template<typename T>
+void mySwap( T &,T &);
+
 int main(){
 	int x, y;
 	string a, b;
@@ -29,4 +32,9 @@ int main(){
 
 	return 0;
 }
-
+ template<typename T>
+  void mySwap( T&x,T&y){
+  	T z=x;
+  	x=y;
+  	y=z;
+  }
