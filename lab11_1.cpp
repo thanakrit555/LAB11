38391bcb2a6a7b194ea74544174b70286ba36d5c
thanakrit555 <59599161+thanakrit555@users.noreply.github.com>
2021-01-15 15:08:43 +0700

    lab11_completed

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,9 +1,19 @@
 #include<iostream>
 using namespace std;
 
+unsigned long long int fibonacci(int );
+
 int main(){
     int x;
     cin >> x;
     cout << fibonacci(x);
     return 0; 
+}
+
+unsigned long long int fibonacci(int n ){
+if( n==0){
+return 0;
+}else if (n==1){
+return 1;
+}else return  fibonacci(n-1) +fibonacci(n-2);
 }
